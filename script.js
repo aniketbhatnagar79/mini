35aa09710fb9a87475b3f889e4e00ad61fd3f072
aniketbhatnagar79 <61017094+aniketbhatnagar79@users.noreply.github.com>
2021-01-27 20:14:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 
 function call(){
-//     fetch('http://27.7.29.206:5000/getalldata')
-//     .then(res=>res.json())
-// .then(data =>{
-//     console.log(data);
-//     // document.getElementById("id1").innerText=data[0].id;
+    fetch('http://115.99.169.87:5000/getalldata')
+    .then(res=>res.json())
+.then(data =>{
+    console.log(data);
+    document.getElementById("id1").innerText=data[0].id;
+    document.getElementById("name1").innerText=data[0].name;
+    document.getElementById("age1").innerText=data[0].age;
+    document.getElementById("desc1").innerText=data[0].desc;
+
+    document.getElementById("id2").innerText=data[1].id;
+    document.getElementById("name2").innerText=data[1].name;
+    document.getElementById("age2").innerText=data[1].age;
+    document.getElementById("desc2").innerText=data[1].desc;
+
+    document.getElementById("id3").innerText=data[1].id;
+    document.getElementById("name3").innerText=data[1].name;
+    document.getElementById("age3").innerText=data[1].age;
+    document.getElementById("desc3").innerText=data[1].desc;
 
     
-// });
-document.getElementById("id1").innerHTML="wkdms";
+});
+
 document.querySelectorAll(".table")[0].style.visibility="visible";
 document.querySelectorAll(".table")[1].style.visibility="visible";
 document.querySelectorAll(".table")[2].style.visibility="visible";
